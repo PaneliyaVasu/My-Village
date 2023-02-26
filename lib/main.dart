f13b989a8c076208f5d11302c1449af7adf5da18
vasu paneliya <paneliyavasu63@gmail.com>
2023-02-26 15:07:14 +0530

    comitted

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,9 @@ import 'package:practice/Drawer/Forget.dart';
 import 'package:practice/register.dart';
 import 'package:practice/theme_model.dart';
 import 'Drawer/update.dart';
-
 import 'package:provider/provider.dart';
-import 'firebase_options.dart';
 import 'home.dart';
-import 'Drawer/Forget.dart';
 import 'login.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,43 +20,42 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-   MyApp ({Key? key}) : super(key: key);
-    final auth=FirebaseAuth.instance;
+  MyApp({Key? key}) : super(key: key);
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-        create:(_)=>ThemeModel(),
-        child : Consumer (builder: (context, ThemeModel themeModal, child){
-    return GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-      home:
-      AnimatedSplashScreen(splash:
-        Image.asset('assest/homelogo.jpg',
-          height: 500,
-          width: 500,
-        ),
-        backgroundColor: Colors.black,
-        duration: 2500,
-        nextScreen:auth.currentUser==null?MyLogin():HomePage(),
-        ),
-
-    routes: {
-      'register': (context) => MyRegister(),
-      'login': (context) => MyLogin(),
-      'home': (context) => HomePage(),
-      'event1':(context) => ED(),
-      'news1':(context) => ND(),
-      'update' :(context)=>UpdatePage(title: '',),
-      'forget' :(context)=>Forget(),
-
-    },
-      themeMode:themeModal.isDark ? ThemeMode.dark : ThemeMode.light,
-      theme: ThemeData(
-        brightness: themeModal.isDark ? Brightness.dark : Brightness.light,
-      ),
-    );
-        }
-        )
-    );
+        create: (_) => ThemeModel(),
+        child: Consumer(builder: (context, ThemeModel themeModal, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: AnimatedSplashScreen(
+              splash: Image.asset(
+                'assest/homelogo.jpg',
+                height: 500,
+                width: 500,
+              ),
+              backgroundColor: Colors.black,
+              duration: 2500,
+              nextScreen: auth.currentUser == null ? MyLogin() : HomePage(),
+            ),
+            routes: {
+              'register': (context) => MyRegister(),
+              'login': (context) => MyLogin(),
+              'home': (context) => HomePage(),
+              'event1': (context) => ED(),
+              'news1': (context) => ND(),
+              'update': (context) => UpdatePage(
+                    title: '',
+                  ),
+              'forget': (context) => Forget(),
+            },
+            themeMode: themeModal.isDark ? ThemeMode.dark : ThemeMode.light,
+            theme: ThemeData(
+              brightness:
+                  themeModal.isDark ? Brightness.dark : Brightness.light,
+            ),
+          );
+        }));
   }
 }
